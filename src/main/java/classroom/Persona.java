a90d29f4974c27f8b5cfbcef35e8126f8122e4c0
Daimon-22 <elvelandiar@unal.edu.co>
2023-03-04 12:22:58 -0500

    intento1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,7 +2,7 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    static long cedula;
     String nombre;
     static int totalPersonas;
     
@@ -12,25 +12,24 @@ public class Persona {
     }
 
     public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
+        this.personas.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
     
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
+        this.personas.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
 
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
+        this.personas.cedula = cedula;
         totalPersonas++;
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre =nombre;
         totalPersonas++;
     }
     
